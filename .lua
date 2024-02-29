b52614d44292304b22dde18beb1e4f074075d303
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-02-29 10:35:12 +0900

    Update .lua

--- .lua
+++ .lua
@@ -10,6 +10,7 @@ https://www.roblox.com/games/15935482021/UPD7-Simulator-Infinity-Anime-Gratis?ga
 https://www.roblox.com/games/1240123653/Zombie-Serangan?gameSearchSessionInfo=18b9355b-0fbd-42f0-bf04-8d8c638e9e9d&isAd=false&nativeAdData=&numberOfLoadedTiles=40&page=searchPage&placeId=1240123653&position=0&universeId=504035427
 https://www.roblox.com/games/4972091010/BARU-Pemberontakan-Zombie?gameSearchSessionInfo=bd8ab1bd-27a2-4f0e-847d-f622377e345b&isAd=false&nativeAdData=&numberOfLoadedTiles=40&page=searchPage&placeId=4972091010&position=0&universeId=1709832923
 https://www.roblox.com/games/12152063271/BARU-Simulator-Pukulan-Bola-Api?gameSetTypeId=100000003&homePageSessionInfo=a6316aa5-e207-4259-b1a8-ccc66946259d&isAd=false&numberOfLoadedTiles=6&page=homePage&placeId=12152063271&position=0&sortPos=0&universeId=4285834347
+https://www.roblox.com/games/14168050172/Simulator-Gauntlet?gameSetTypeId=100000003&homePageSessionInfo=7d1834d6-9512-4dbc-ad60-c6386c262834&isAd=false&numberOfLoadedTiles=6&page=homePage&placeId=14168050172&position=0&sortPos=0&universeId=4903983422
 ]]
 
 if game.PlaceId == 13885546444 then -- mowing simulator
@@ -36,6 +37,8 @@ elseif game.PlaceId == 4972091010 then -- Zombie Uprising
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/TurtleHub/main/Zombie-Uprising/Script.lua"))()
 elseif game.PlaceId == 12152063271 then -- fire ball punch
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/FireballPunchSimulator/main/.lua"))()
+elseif game.PlaceId == 14168050172 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/Gauntlet-Simulator/main/.lua"))()
 else
   local hin = Instance.new("Hint",game:GetService("Workspace"))
   hin.Text = "Unsupported games"
